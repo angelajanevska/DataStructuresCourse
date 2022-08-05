db3062b42377361481927e040e708a270c720f08
angelajanevska <80795984+angelajanevska@users.noreply.github.com>
2022-08-05 12:23:37 +0200

    home page

--- courseproject/urls.py
+++ courseproject/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -24,4 +26,4 @@ urlpatterns = [
     path('logout/', logout_user, name="logout_user"),
     path('register/', register, name="register"),
     path('', home, name="home"),
-]
+]+static('media/', document_root=settings.MEDIA_ROOT)
